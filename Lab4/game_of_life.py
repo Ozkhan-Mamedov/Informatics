e63fda4def1ac0ed3f55e114a499f05e2f7584d4
Ozkhan Mamedov <ozhanm96@yandex.ru>
2017-04-13 13:38:50 +0300

    Lab4
    Lab4

---
+++ Lab4/game_of_life.py
@@ -0,0 +1,49 @@
+file = "life.txt"
+field = []
+field2 = []
+
+def numNear (r,c):
+	N = 0
+	for a in range (r-1, r+2):
+		for b in range (c-1, c+2):
+			if a == r and b == c:
+				continue
+			if field[a][b] == '*':
+				N += 1
+	return N
+
+F = open ("file.txt", "r")
+for line in F:
+	L = []
+	L2 = []
+	for a in line:
+		if a == '\r' or a == '\n':
+			continue
+		L.append (a)
+		L2.append (a)
+	field.append(L)
+	field2.append(L2)
+F.close()
+
+for a in field:
+	s = ""
+	for b in a:
+		s += b;
+	print (s)
+
+for a in range (1, len(field)-1):
+	for b in range (1, len(field[a])-1):
+		n = numNear (a,b)
+		field2[a][b] = str (n)
+		#
+
+print ("\n")
+for a in field2:
+	s = ""
+	for b in a:
+		s += b;
+	print (s)
+
+#for a in range (1, len(field)-1):
+#	for b in range (1, len(field[a])-1):
+#		field [a][b] = field2 [a][b]
